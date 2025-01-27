0ab23c714a212a365b5dcf51882526e6a9b2f419
ishan-159 <ishantiwari159@gmail.com>
2025-01-27 10:32:54 +0530

    I am done with bank system problem.

---
+++ Day6/Day6/src/problemstatement/banksystem/Lonable.java
@@ -0,0 +1,8 @@
+package problemstatement.banksystem;
+
+// Loanable.java (Interface)
+interface Loanable {
+    void applyForLoan(double loanAmount);
+
+    double calculateLoanEligibility();
+}
---
+++ Day6/Day6/src/problemstatement/banksystem/Main.java
@@ -0,0 +1,28 @@
+package problemstatement.banksystem;
+
+// Main.java (Demo Class)
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<BankAccount> accounts = new ArrayList<>();
+
+        accounts.add(new SavingsAccount("SA123", "Alice", 1000.0));
+        accounts.add(new CurrentAccount("CA456", "Bob", 2000.0, 500.0));
+
+        for (BankAccount account : accounts) {
+            System.out.println("Account Holder: " + account.getHolderName());
+            System.out.println("Account Balance: $" + account.getBalance());
+            System.out.println("Interest Earned: $" + account.calculateInterest());
+
+            if (account instanceof Loanable) {
+                Loanable loanable = (Loanable) account;
+                loanable.applyForLoan(5000.0);
+                System.out.println("Loan Eligibility: $" + loanable.calculateLoanEligibility());
+            }
+
+            System.out.println("-----------------------");
+        }
+    }
+}
